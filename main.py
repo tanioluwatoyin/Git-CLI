ef5109f9af92a25256a0803055fb42560f440366
oluwatoyin ibrahim <105392364+tanioluwatoyin@users.noreply.github.com>
2022-06-08 11:56:01 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+# import randoms
+
+"def" #play_games:
+"" 
+"def" #import randoms games
+
+"create" #games 
+#games (R=rock)
+#games (P=paper)
+#games (S=scissors)
+
+
+"" #games functions (R+P+S)
+"cpu to pick game functions = R  "
+"player to pick game function = S "
+from ast import Str
+from multiprocessing import RLock
+from tkinter import colorchooser
+from tkinter.colorchooser import Chooser
+
+
+(input) = "cpu to pick game function"
+(Str) = "player to pick game function"
+(tie) = "input + str "
+
+#check win (cpu , player)
+("cpu win == comp win")
+("player win == you win")
+
+   #(function to check the winner)
+"if cpu == player"
+print ("tie")
+"if cpu == R and player == S"
+print ("cpu wins")
+" if cpu == p and player== S"
+print('You win')
+"if cpu == S and player == R"
+print("comp win")     
+ 
+
+ #gameDict = {
+ # R == "Rock
+ # P == "Paper
+ # S == Scissors
+ # "}
+     
+"win_checker" ("choice" , "inp")
+# def input_validator(inp,choice)
+
+"if inp == R or inp == S or inp == P"
+"chooser(inp, choice)"
+"else" 
+print("wrong choice: Try again !!!")
+
+
+
+(exit)
+
+    
+
+
+ 
+
